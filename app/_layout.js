aa951445213e9bddf62f0ebadf38177885f43fb7
ali ouchagour <ali.ouchagour01@gmail.com>
2023-11-24 19:48:18 +0100

    adding font using useFounts hook

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,29 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
 
+// SplashScreen.preventAutoHideAsync();
 
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "home",
+};
 
 const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+  console.log(fontsLoaded);
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-  <Stack/>
+     <Stack />
+
   )
-}
- export default Layout;
+};
+
+export default Layout;
