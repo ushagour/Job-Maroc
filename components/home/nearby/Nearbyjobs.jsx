152dbb0f005fae8f5e927e4e352af9023ad64c9c
ali ouchagour <ali.ouchagour01@gmail.com>
2024-01-03 20:29:26 +0100

    like buttonin nearby component

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -11,16 +11,48 @@ import styles from "./nearbyjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import NearbyjobCard from "../../common/cards/nearby/NearbyJobCard";
 import useFetch from "../../../hook/useFetch";
+import { useCallback, useState } from "react";
 
 const Nearbyjobs = (props) => {
   const router = useRouter();
+
+  // Initialize likedJobs as an object with job_id as keys
+  const [likedJobs, setLikedJobs] = useState({});
+
 
   const { data, isLoading, error } = useFetch("search", {
     query: `NearBay jobs for software developers in ${props.city}`,
     num_pages: "1",
     radius: '1',
-
   });
+
+  const handleLikeButtonPress = (jobId) => {
+    // Toggle the liked status for the specific job
+    setLikedJobs((prevLikedJobs) => ({
+      ...prevLikedJobs,
+      [jobId]: !prevLikedJobs[jobId],
+    }));
+
+    // Save liked job to the database (if needed)
+    saveLikedJobs(jobId);
+  };
+
+  const saveLikedJobs = async (jobId) => {
+    try {
+      
+      console.log(`Job ${jobId} liked`);
+     useEffect (() => {
+           addDoc(collection (FIRESTORE_DB, 'todos'), { title: 'I am a test', done: false });
+            }, []);
+
+
+      //send updates to the database 
+      // Optionally, make your database save request here
+    } catch (error) {
+      console.error('Error saving liked job:', error.message);
+    }
+  };
+
 
   return (
     <View style={styles.container}>
@@ -42,7 +74,12 @@ const Nearbyjobs = (props) => {
             job={job}
             key={`nearby-job-${job?.job_id}`}
             handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+            handleLikeButtonPress={()=>{handleLikeButtonPress(job?.job_id)}}
+            isLiked={likedJobs[job?.job_id] || false}//the prop isliked containe the value true or false
+
           />)
+
+
           }
            
           )
